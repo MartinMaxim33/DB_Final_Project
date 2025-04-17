622cef2e322d827f0ac02fc19ae0aec1fed4ec5e
MartinMaxim33 <maxmt-25@rhodes.edu>
2025-04-17 16:55:23 -0500

    all the tables in

--- Milestone 3+4/test_tables.py
+++ Milestone 3+4/test_tables.py
@@ -33,19 +33,44 @@ def list_championships():
         print(f"  MVP Player ID: {champ['mvp']}")
         print("-" * 40)
 
-def add_courses_from_csv(filename):
-    with open(filename, 'r') as file:
-        with cur.copy(f"COPY courses FROM STDIN WITH (FORMAT CSV, HEADER true)") as copy:
-            copy.write(file.read())
-    conn.commit()
+def list_teams():
+    cur.execute("SELECT * FROM Teams")
+    rows = cur.fetchall()
+    print("Here are the teams:")
+    for team in rows:
+        print(f"Team Name: {team['t_name']}, City: {team['city']}, State: {team['state']}, League: {team['league']}")
 
-def delete_all_players():
-    cur.execute("DELETE FROM player")  # careful! deletes everything.
-    conn.commit()
+def list_venues():
+    cur.execute("SELECT * FROM Venues")
+    rows = cur.fetchall()
+    print("Here are the venues:")
+    for venue in rows:
+        print(f"Venue Name: {venue['name']}, City: {venue['city']}, Type: {venue['type']}, Capacity: {venue['capacity']}")
+
+def list_games():
+    cur.execute("SELECT g.date, g.time, t1.t_name AS team1, t2.t_name AS team2, g.score, v.name AS venue FROM Games g \
+                 JOIN Teams t1 ON g.team1 = t1.team_id \
+                 JOIN Teams t2 ON g.team2 = t2.team_id \
+                 JOIN Venues v ON g.venue = v.name")
+    rows = cur.fetchall()
+    print("Here are the games:")
+    for game in rows:
+        print(f"Date: {game['date']}, Time: {game['time']}, {game['team1']} vs {game['team2']}, Score: {game['score']}, Venue: {game['venue']}")
+
+def list_leagues():
+    cur.execute("SELECT * FROM Leagues")
+    rows = cur.fetchall()
+    print("Here are the leagues:")
+    for league in rows:
+        print(f"League Name: {league['name']}, Commissioner: {league['commissioner']}, Net Worth: ${league['net_worth']}")
 
 def main():
     list_player()
     list_championships()
+    list_teams()
+    list_venues()
+    list_games()
+    list_leagues()
     ##delete_all_courses()
     ##add_courses_from_csv("courses.csv")
     ##list_player()
