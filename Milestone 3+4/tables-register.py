bca19729dca885c581d31f6fe21579ebf3f674ff
Jud Turner <turnerjudson@gmail.com>
2025-04-20 15:11:08 -0500

    added home pages

--- Milestone 3+4/tables-register.py
+++ Milestone 3+4/tables-register.py
@@ -9,16 +9,28 @@ conn = psycopg.connect(f"host=dbclass.rhodescs.org dbname=practice user={DBUSER}
 # Open a cursor to perform database operations
 cur = conn.cursor(row_factory=dict_row)
 
-
-
 @ui.page('/')
 def homepage():
     ui.label("Welcome to the homepage!")
-    ui.link("NFL")
-    ui.link("NHL")
-    ui.link("NBA")
-    ui.link("MLB")
+    ui.link("NFL", "/nfl")
+    ui.link("NHL", "/nhl")
+    ui.link("NBA", "/nba")
+    ui.link("MLB", "/mlb")
 
+@ui.page('/nfl')
+def nfl_page():
+    ui.label("NFL Home Page")
 
+@ui.page('/nhl')
+def nhl_page():
+    ui.label("NHL Home Page")
+
+@ui.page('/nba')
+def nba_page():
+    ui.label("NBA Home Page")
+
+@ui.page('/mlb')
+def mlb_page():
+    ui.label("MLB Home Page")
 
 ui.run(reload=False)
